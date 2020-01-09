7ca7bcfdce7a387c305f68bfb5a9f57289dcaf0e
Félix Turgeon <felixturgeonstudies@gmail.com>
2020-01-09 13:14:46 -0500

    added function to close mock browser

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 document.getElementById("urlGoesHere").innerText = document.URL;
+
+document.getElementById("closeButton").addEventListener("click", closeTab);
+document.getElementById("shortcut").addEventListener("dblclick", closeTab);
+
+function closeTab(){
+    var window = document.getElementById("window");
+    var shortcut = document.getElementById("shortcut");
+
+    if(window.style.display === "block"){
+        window.style.display = "none";
+        shortcut.style.display ="block";
+    } else if(window.style.display === "none"){
+        shortcut.style.display ="none";
+        window.style.display ="block";
+    }
+
+
+}
